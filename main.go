3355ab8df3596dd8b78904bb4f5fadf5477aaf7c
Naren Mudivarthy <narenuday595@gmail.com>
2018-02-12 14:08:03 -0800

    Updated github tocken

--- main.go
+++ main.go
@@ -21,15 +21,15 @@ type Package struct {
 func main() {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "be97ba44f3477abb9c403a0d94baab8a29851f5d"},
+		&oauth2.Token{AccessToken: "1161bc4a9c064328d5693421207ae717c129ffec"},
 	)
+
 	tc := oauth2.NewClient(ctx, ts)
 	// get go-github client
 	client := github.NewClient(tc)
-	fmt.Println(client)
 
-	// repo, _, err := client.Repositories.Get(ctx, "Golang-Coach", "Lessons")
-	repo, _, err := client.Repositories.Get(ctx, "naren-m", "dotfiles")
+	repo, _, err := client.Repositories.Get(ctx, "Golang-Coach", "Lessons")
+	//	repo, _, err := client.Repositories.Get(ctx, "naren-m", "dotfiles")
 
 	if err != nil {
 		fmt.Printf("Problem in getting repository information %v\n", err)
